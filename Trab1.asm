1bf03b07a22c8731f69cd1b5547d28e2ad2e5bb8
felipecosta <felipedf7@gmail.com>
2016-09-13 09:48:40 -0300

    Adicionada subotina Imprime_Matriz

--- Trab1.asm
+++ Trab1.asm
@@ -9,6 +9,7 @@ elemento: .asciiz "Elemento "
 barra: .asciiz "-"
 dois_pontos: .asciiz ": "
 quebra_linha: .asciiz "\n"
+space: .asciiz "  "
 
 
 .text
@@ -80,11 +81,87 @@ sw $v0, matrix($t4)
 addi $t2, $t2, 1 # Vai pro proximo elemento da linha
 bne $t2, $t0, LOOP_LINHA
 
-addi $t3, $t3, 1 # Vai pra proxima coluna
 li $t2, 0 # Volta pro primeiro elemento da linha
+addi $t3, $t3, 1 # Vai pra proxima coluna
 bne $t3, $t1, LOOP_LINHA
 
+# Nesse ponto a matriz ja esta pronta, $t0 guarda o numero de linhas e $t1 o numero de colunas
+move $a0, $t0
+move $a1, $t1
+jal IMPRIME_MATRIZ
+j EXIT
 
+
+
+
+# Subrotina que imprime a matriz
+# Recebe $a0 = Numero de Linhas, $a1 = Numero de Colunas
+IMPRIME_MATRIZ:
+# Stacking
+addi $sp, $sp, -24
+sw $t0, 0($sp)
+sw $t1, 4($sp)
+sw $t2, 8($sp)
+sw $t3, 12($sp)
+sw $t4, 16($sp)
+sw $t5, 20($sp)
+
+
+# Body
+move $t0, $a0
+move $t1, $a1
+
+li $t2, 0 # Contador de linhas
+li $t3, 0 # Contador de colunas
+
+
+
+IMPRIME_MATRIZ_LOOP: # Loop da funcao
+
+# Calculando linha do elemento
+sll $t4, $t2, 2
+
+# Calculando coluna
+sll $t5, $t3, 2
+mult $t5, $t1
+mflo $t5
+
+add $t4, $t4, $t5
+lw $t4, matrix($t4) # Carrega valor que vai ser impresso em lw
+
+# Imprime valor + espaco
+li $v0, 1
+move $a0, $t4
+syscall
+
+li $v0, 4
+la $a0, space
+syscall
+
+addi $t2, $t2, 1 # Vai pro proximo elemento da linha
+bne $t2, $t0, IMPRIME_MATRIZ_LOOP # Se n tiver acabado volta pro loop
+
+# Imprime quebra de linha pra mudar de coluna
+li $v0, 4
+la $a0, quebra_linha
+syscall
+
+addi $t3, $t3, 1 # Muda a coluna
+li $t2, 0 # Zera o contador do elemento na linha
+bne $t3, $t1, IMPRIME_MATRIZ_LOOP # Se n tiver acabado volta pro loop
+
+#Unstacking
+lw $t0, 0($sp)
+lw $t1, 4($sp)
+lw $t2, 8($sp)
+lw $t3, 12($sp)
+lw $t4, 16($sp)
+lw $t5, 20($sp)
+addi $sp, $sp, 24
+
+jr $ra # Fim da funcao
+
+EXIT:
 
 
 
