71a8a9ea9fa34b0a535946e31f612b1e898776b9
felipecosta <felipedf7@gmail.com>
2016-09-12 21:56:10 -0300

    Input da matriz funcionando

--- Trab1.asm
+++ Trab1.asm
@@ -1,8 +1,10 @@
+# Autores : Felipe da Costa Malaquias e Lucas Mota Ribeiro
+# OAC 2/2016
+
 .data
 matrix: .space 36
 msg1: .asciiz "Qual o numero de linhas? <ENTER>\n"
 msg2: .asciiz "Qual o numero de colunas? <ENTER>\n"
-msg3: .asciiz "Introduza as componentes separadas por <TAB> e por linha <ENTER>:\n"
 elemento: .asciiz "Elemento "
 barra: .asciiz "-"
 dois_pontos: .asciiz ": "
@@ -30,16 +32,14 @@ syscall
 
 add $t1, $v0, $zero # Guarda o valor no t1
 
-# Pega os valores da matriz usando espaco para novo valor
-# na msm linha e enter para nova linha
-# Valor1 <SPACE> Valor2 <SPACE> Valor3 <ENTER>
+# Pega os valores da matriz um por vez
 
 li $t2, 0 # Loop counter para linha
 li $t3, 0 # Loop counter para coluna
 
-
-
+# Loop que pega uma linha inteira da matriz
 LOOP_LINHA:
+# Imprimindo Elemento x-y: 
 li $v0,4
 la $a0, elemento
 syscall
@@ -59,24 +59,30 @@ syscall
 li $v0, 4
 la $a0, dois_pontos
 syscall
+# Acabou de imprimir Elemento x-y:
 
+# Pega o elemento
 li $v0, 5
 syscall
 
-sll $s2, $t2, 2
-mul
-add $s0, $s2, $zero
-sw $v0, matrix($s0)
-addi $t2, $t2, 1
+# Posicao do proximo elemento na linha em words
+sll $s0, $t2, 2 
+
+# Calculando o offset pro numero cair na coluna certa
+sll $s1, $t1, 2
+mult $s1, $t3
+mflo $s1
+
+# Soma da posicao na linha + offset
+add $t4, $s0, $s1
+sw $v0, matrix($t4)
+
+addi $t2, $t2, 1 # Vai pro proximo elemento da linha
 bne $t2, $t0, LOOP_LINHA
 
-li $t2, 0
-addi $t3, $t3, 1
+addi $t3, $t3, 1 # Vai pra proxima coluna
+li $t2, 0 # Volta pro primeiro elemento da linha
 bne $t3, $t1, LOOP_LINHA
-
-
-
-
 
 
 
